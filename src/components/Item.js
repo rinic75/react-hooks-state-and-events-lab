dea02fe35ef476309bb77ec774c34393ec3315e0
Chang Shin <cshin7374@gmail.com>
2022-07-11 20:15:46 -0500

    commit tree d65a87692b8931b3ad88c0ba079a1ea4ebe1e9e8

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState(false)
+
+  function handleClick () {
+    setCart(cart => !cart)
+  }
+
   return (
-    <li className="">
+    <li className={cart? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}> {cart? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
